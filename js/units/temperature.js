6b193aa7b474169353dba32679e0e3e403349aea
bitzsalex <mblonglive@gmail.com>
2023-11-07 21:45:52 +0900

    Add fahrenheit conversion functions

--- js/units/temperature.js
+++ js/units/temperature.js
@@ -1,18 +1,18 @@
 // celsius conversion functions
 const cToDe = (value, reverse = false) => {
-	return reverse ? (value + 100) / 1.5 : (value * 1.5) - 100
+	return reverse ? (value + 100) / 1.5 : value * 1.5 - 100
 }
 
 const cToF = (value, reverse = false) => {
-	return reverse ? (value - 32) * 5 / 9 : (value * 9 / 5) + 32
+	return reverse ? ((value - 32) * 5) / 9 : (value * 9) / 5 + 32
 }
 
 const cToK = (value, reverse = false) => {
-	return reverse ? (value - 273.15) : (value + 273.15)
+	return reverse ? value - 273.15 : value + 273.15
 }
 
 const cToR = (value, reverse = false) => {
-	return reverse ? (value - 491.67) * 5 / 9 : (value * 9 / 5) + 491.67
+	return reverse ? ((value - 491.67) * 5) / 9 : (value * 9) / 5 + 491.67
 }
 
 const cToN = (value, reverse = false) => {
@@ -29,11 +29,17 @@ const deToR = (value, reverse = false) => {}
 const deToN = (value, reverse = false) => {}
 
 // fahrenheit conversion functions
-const fToK = (value, reverse = false) => {}
+const fToK = (value, reverse = false) => {
+	return reverse ? (value - 273.15) * 9 / 5 + 32 : (value - 32) * 5 / 9 + 273.15
+}
 
-const fToR = (value, reverse = false) => {}
+const fToR = (value, reverse = false) => {
+	return reverse ? value - 459.67 : value + 459.67
+}
 
-const fToN = (value, reverse = false) => {}
+const fToN = (value, reverse = false) => {
+	return reverse ? (value * 5.45454555) + 32 : (value - 32) / 5.45454555
+}
 
 // kelvin conversion functions
 const kToR = (value, reverse = false) => {}
@@ -42,7 +48,6 @@ const kToN = (value, reverse = false) => {}
 
 // rankine conversion functions
 const rToN = (value, reverse = false) => {}
-
 
 function Temperature() {
 	this.celsius = {
@@ -58,7 +63,9 @@ function Temperature() {
 	this.delisle = {
 		symbol: "De",
 		supPre: "o",
-		toCelsius(value) { return cToDe(value, true) },
+		toCelsius(value) {
+			return cToDe(value, true)
+		},
 		toFahrenheit: deToF,
 		toKelvin: deToK,
 		toRankine: deToR,
@@ -68,8 +75,12 @@ function Temperature() {
 	this.fahrenheit = {
 		symbol: "F",
 		supPre: "o",
-		toCelsius(value) { return cToF(value, true) },
-		toDelisle(value) { return deToF(value, true) },
+		toCelsius(value) {
+			return cToF(value, true)
+		},
+		toDelisle(value) {
+			return deToF(value, true)
+		},
 		toKelvin: fToK,
 		toRankine: fToR,
 		toNewton: fToN,
@@ -77,9 +88,15 @@ function Temperature() {
 
 	this.kelvin = {
 		symbol: "K",
-		toCelsius(value) { return cToK(value, true) },
-		toDelisle(value) { return deToK(value, true) },
-		toFahrenheit(value) { return fToK(value, true) },
+		toCelsius(value) {
+			return cToK(value, true)
+		},
+		toDelisle(value) {
+			return deToK(value, true)
+		},
+		toFahrenheit(value) {
+			return fToK(value, true)
+		},
 		toRankine: kToR,
 		toNewton: kToN,
 	}
@@ -87,21 +104,39 @@ function Temperature() {
 	this.rankine = {
 		symbol: "R",
 		supPre: "o",
-		toCelsius(value) { return cToR(value, true) },
-		toDelisle(value) { return deToR(value, true) },
-		toFahrenheit(value) { return fToR(value, true) },
-		toKelvin(value) { return kToR(value, true) },
+		toCelsius(value) {
+			return cToR(value, true)
+		},
+		toDelisle(value) {
+			return deToR(value, true)
+		},
+		toFahrenheit(value) {
+			return fToR(value, true)
+		},
+		toKelvin(value) {
+			return kToR(value, true)
+		},
 		toNewton: rToN,
 	}
 
 	this.newton = {
 		symbol: "N",
 		supPre: "o",
-		toCelsius(value) { return cToN(value, true) },
-		toDelisle(value) { return deToN(value, true) },
-		toFahrenheit(value) { return fToN(value, true) },
-		toKelvin(value) { return kToN(value, true) },
-		toRankine(value) { return rToN(value, true) },
+		toCelsius(value) {
+			return cToN(value, true)
+		},
+		toDelisle(value) {
+			return deToN(value, true)
+		},
+		toFahrenheit(value) {
+			return fToN(value, true)
+		},
+		toKelvin(value) {
+			return kToN(value, true)
+		},
+		toRankine(value) {
+			return rToN(value, true)
+		},
 	}
 
 	this.defaults = ["celsius", "fahrenheit", "kelvin"]
