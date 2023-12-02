a40f446947e2da92b8517a26563beea1fdfd9557
bitzsalex <mblonglive@gmail.com>
2023-12-02 20:34:57 +0900

    Populate unit options into view

--- js/units.js
+++ js/units.js
@@ -1,21 +1,36 @@
 import Area from "./units/area.js"
 import Data from "./units/data.js"
 import Length from "./units/length.js"
+import Weight from "./units/weight.js"
 import Speed from "./units/speed.js"
 import Temperature from "./units/temperature.js"
 import Time from "./units/time.js"
 import Volume from "./units/volume.js"
-import Weight from "./units/volume.js"
 import * as helper from "./helper.js"
 
-const MAX_SELECT_ITEM_NUMS = 8
+const MAX_SELECT_ITEM_NUMS = 9
+
+const units = {
+	list: {
+		Area: Area,
+		Data: Data,
+		Length: Length,
+		"Mass/Weight": Weight,
+		Speed: Speed,
+		Temperature: Temperature,
+		Time: Time,
+		Volume: Volume,
+	},
+	default: "Mass/Weight",
+}
 
 const body = document.body
 const selects = document.querySelectorAll(".select")
-const selectOptions = document.querySelectorAll(".select__option")
+const unitSelect = document.querySelector(".select.select--unit")
 const unitInputs = document.querySelectorAll(".unit__input")
 
 let selectedUnitInputCurrentValue = ""
+let selectedUnit = null
 
 const removeSelectedOption = (parent) => {
 	for (let option of parent.children) option.classList.remove("selected")
@@ -57,10 +72,6 @@ selects.forEach((select) => {
 	})
 })
 
-selectOptions.forEach((option) => {
-	option.addEventListener("click", selectOption)
-})
-
 unitInputs.forEach((unitInput) => {
 	unitInput.addEventListener("input", (event) => {
 		event.stopPropagation()
@@ -72,3 +83,68 @@ unitInputs.forEach((unitInput) => {
 // this event is to hide the visible select option
 body.addEventListener("click", closeSelectFromOutside)
 
+const createSelectOptions = (parent, list, defaultOption, eventCallback) => {
+	let selected = null
+	parent.innerHTML = ""
+	let children = []
+
+	for (let item of list) {
+		let li = document.createElement("li")
+		li.classList.add("select__option")
+		li.innerText = item
+		li.addEventListener("click", selectOption)
+		li.addEventListener("click", eventCallback)
+
+		if (item === defaultOption) {
+			li.classList.add("selected")
+			selected = li
+		}
+		parent.append(li)
+		children.push(li)
+	}
+
+	if (selected) {
+		selected.click()
+		closeSelectFromOutside()
+	}
+
+	return children
+}
+
+const changeUnit = () => {
+	console.log("changing the unit")
+}
+
+const changeToSelectedUnit = (event) => {
+	event.stopPropagation()
+	let selected = event.target.innerText
+	selectedUnit = new units.list[selected]()
+
+	if (selected === units.default && localStorage.getItem("lastUnitChoice")) localStorage.removeItem("lastUnitChoice")
+	else localStorage.setItem("lastUnitChoice", selected)
+
+	populateSelectedUnitOptions()
+}
+
+const populateUnitOptions = () => {
+	let userUnitChoice = localStorage.getItem("lastUnitChoice")
+	userUnitChoice = userUnitChoice ?? units.default
+	selectedUnit = new units.list[userUnitChoice]()
+
+	let unitSelectOptions = unitSelect.querySelector("ul.select__options")
+	createSelectOptions(unitSelectOptions, Object.keys(units.list), userUnitChoice, changeToSelectedUnit)
+}
+
+const populateSelectedUnitOptions = () => {
+	let unitSelects = document.querySelectorAll("div.screen__unit .select")
+	let defaults = selectedUnit.defaults
+	let keys = Object.keys(selectedUnit).filter((item) => item !== "defaults")
+
+	unitSelects.forEach((innerSelect, index) => {
+		let selectOptions = innerSelect.querySelector("ul.select__options")
+		createSelectOptions(selectOptions, keys, defaults[index], changeUnit)
+	})
+}
+
+// populating the unit select options
+populateUnitOptions()
